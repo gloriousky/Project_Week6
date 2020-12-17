dfbe522858e82a64018c2a9bfe6e0db2d999ab1f
David <sky80420@hotmail.com>
2020-12-17 15:57:29 +0800

    Week6-version1
    Scss bootstrap  login頁面js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -220,4 +220,16 @@ gsap.from(".js-subscribe-text", {
     trigger: ".js-subscribe"
   }
 });
+
+
+
+$(window).scroll(function (e) {  
+  if ($(window).scrollTop() <= 0) {
+    $('.navbar').addClass('bg-white');
+    $('.navbar').removeClass('bg-light');
+  } else {
+    $('.navbar').addClass('bg-light');
+    $('.navbar').removeClass('bg-white');
+  }
+});
 //# sourceMappingURL=all.js.map
