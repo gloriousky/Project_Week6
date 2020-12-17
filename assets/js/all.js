7d0e9b980d43e91a2502aa35953b7adb0e7e7902
David <sky80420@hotmail.com>
2020-12-17 15:58:14 +0800

    Update 2020-12-17T07:58:10.041Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -220,6 +220,15 @@ gsap.from(".js-subscribe-text", {
     trigger: ".js-subscribe"
   }
 });
+$(window).scroll(function (e) {
+  if ($(window).scrollTop() <= 0) {
+    $('.navbar').addClass('bg-white');
+    $('.navbar').removeClass('bg-light');
+  } else {
+    $('.navbar').addClass('bg-light');
+    $('.navbar').removeClass('bg-white');
+  }
+});
 "use strict";
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
